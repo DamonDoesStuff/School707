ec8c97973c90af78649034c5507f70713102939d
DamonRochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-11 13:18:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('button').addEventListener('click', calculate)
+
+let firstNum = 0
+let secondNum = 0
+let counter = 0
+let answer = 0
+
+function calculate () {
+  firstNum = document.getElementById('triple777').value
+  secondNum = document.getElementById('darkie').value
+  firstNum = parseInt(firstNum)
+  secondNum = parseInt(secondNum)
+
+  for (counter = 0; counter < firstNum; counter++)
+  {
+    answer = answer + secondNum
+  }
+  document.getElementById('answer').innerHTML = answer
+}
